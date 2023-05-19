dfd5cec5f2ac8250930411f572361c8d405bc3ae
omartins-zs <gabrielmatheus209@gmail.com>
2023-05-19 15:53:48 -0300

    Add Alter da Table usuarios add Column celular

--- script.sql
+++ script.sql
@@ -6,3 +6,5 @@ CREATE TABLE `sms_aniversariantes`.`usuarios` ( `id` INT NOT NULL AUTO_INCREMENT
 
 
 INSERT INTO `usuarios` (`id`, `nome`, `email`, `data_nascimento`) VALUES (NULL, 'Gabriel', 'gabriel@email.com', '2023-05-19'), (NULL, 'Lucas', 'lucas@email.com', '2023-05-19'), (NULL, 'Pedro', 'pedro@email.com', '2023-05-19'), (NULL, 'Fernando', 'fernando@email.com', '2023-05-21'), (NULL, 'Nicoly', 'nicoly@email.com', '2023-05-21')
+
+ALTER TABLE `usuarios` ADD `celular` VARCHAR(22) NOT NULL AFTER `data_nascimento`;
