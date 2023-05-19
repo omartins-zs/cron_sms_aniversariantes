4c3da18116904359150265030320c47c507dc221
omartins-zs <gabrielmatheus209@gmail.com>
2023-05-19 15:21:47 -0300

    Organizando e Imprimindo os valores na Tela

--- index.php
+++ index.php
@@ -20,14 +20,24 @@ include_once './conexao.php';
     <?php
     $query_usuarios = "SELECT id, nome, email, data_nascimento
                          FROM usuarios
-                     /* WHERE id = 100 */";
+                        WHERE DAY(data_nascimento) = DAY(CURDATE())
+                          AND MONTH (data_nascimento) = MONTH(CURDATE())
+                        LIMIT 1
+                         ";
 
     $result_usuarios =  $conn->prepare($query_usuarios);
     $result_usuarios->execute();
 
     if (($result_usuarios) and ($result_usuarios->rowCount() != 0)) {
         while ($row_usuario = $result_usuarios->fetch(PDO::FETCH_ASSOC)) {
-            var_dump(($row_usuario));
+            extract($row_usuario);
+            echo "ID: $id <br>";
+            echo "Nome: $nome <br>";
+            echo "E-mail: $email <br>";
+            echo "Data de nascimento: " . date('d/m/Y', strtotime($data_nascimento)) . " <br>";
+            echo "<hr>";
+
+            // var_dump(($row_usuario));
         }
     } else {
         echo  "<p style='color: #f00;'>Nenhum aniversariante encontrado</p>";
